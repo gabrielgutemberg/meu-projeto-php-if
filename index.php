962a60f95e9431b789f4e80c7afb809101d73477
gabrielgutemberg <60550099+gabrielgutemberg@users.noreply.github.com>
2026-02-05 19:38:48 -0300

    Aula 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha Primeira Pagina</title>
+</head>
+<body>
+    <h1>
+        Minha Primeira Pagina
+    </h1>
+</body>
+</html>
