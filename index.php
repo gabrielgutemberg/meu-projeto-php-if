b158b08a323aac27b30ea8f6454e95411bfdedf6
gabrielgutemberg <60550099+gabrielgutemberg@users.noreply.github.com>
2026-02-05 21:39:33 -0300

    AULA 2

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha Primeira Pagina</title>
+    <title>Minha primeira página</title>
+    <link rel="stylesheet" href="assets/styles.css">
 </head>
+
 <body>
-    <h1>
-        Minha Primeira Pagina
-    </h1>
+    <div class="container">
+
+        <!-- LINGUAGEM PHP -->
+        <?php
+        $curso = "Sistemas";
+        $estudante = "Jonh Smith";
+
+        ?>
+
+        <!-- CABEÇALHO DA MINHA PÁGINA  -->
+        <header>
+            <!-- IMAGEM -->
+            <img src="assets/ifto-logo.png" alt="ifto-logo">
+
+            <!-- TÍTULO -->
+            <h1>Esse é meu primeiro projeto WEB</h1>
+            <!-- PARÁGRAFO DE DESCRIÇÃO -->
+            <p>Me chamo <?php echo $estudante; ?> aluno do 3° período de <?php echo $curso ?></p>
+        </header>
+
+        <!-- MENU DE NAVEGAÇÃO -->
+        <nav>
+            <!-- LISTA DESORDENADA -->
+            <ul>
+                <li>
+                    <a href="tags.html">Tags HTML</a>
+                </li>
+                <li>
+                    <a href="css.html">CSS</a>
+                </li>
+            </ul>
+
+        </nav>
+
+
+
+    </div>
+
 </body>
+
 </html>
